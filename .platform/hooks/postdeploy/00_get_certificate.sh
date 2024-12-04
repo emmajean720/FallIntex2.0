2c6ac9456090b3b639de7a9f23a92d3ae2466b4f
emmajean720 <167582619+emmajean720@users.noreply.github.com>
2024-12-03 20:09:19 -0700

    change to elastic beanstalk name in https config file

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d myturtleshellproject.is404.net --nginx --agree-tos --email ejpratt7@byu.edu
+sudo certbot -n -d myturtleproj-env.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email ejpratt7@byu.edu
