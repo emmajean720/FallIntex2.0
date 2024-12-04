a932ea882424611674f7f5b9664edfca32d33c67
emmajean720 <167582619+emmajean720@users.noreply.github.com>
2024-12-04 11:30:12 -0700

    fixed

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d myturtleproj.is404.net --nginx --agree-tos --email ejpratt7@byu.edu
+sudo certbot -n -d  --nginx --agree-tos --email ejpratt7@byu.edu
